db7a8bb7e248281e7c4d07a5ad715ca9efb6937f
Jeff Horn <theverything@BigMac.local>
2013-01-25 19:46:47 -0800

    removed static assests and add quotes#index to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Quotes::Application.routes.draw do
-  get "static/index"
-
-  get "quotes/index"
+  #get "quotes/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +50,7 @@ Quotes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'quotes#index'
 
   # See how all your routes lay out with "rake routes"
 
