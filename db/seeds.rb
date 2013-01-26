044facdac89a48bb1265fe3586900f7e784eb65d
Jeff Horn <theverything@BigMac.local>
2013-01-25 19:08:13 -0800

    add database seed content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ Quote.create([
+
+  {text: "A person who never made a mistake never tried anything new.", author: "Albert Einstein"}, 
+  {text: "In the end, it's not the years in your life that count. It's the life in your years.", author: "Abraham Lincoln"}, 
+  {text: "If you can dream it, you can do it.", author: "Walt Disney"}, 
+  {text: "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.", author: "Barack Obama"}, 
+  {text: "My favorite things in life don't cost any money. It's really clear that the most precious resource we all have is time.", author: "Steve Jobs"}, 
+  {text: "Failure is simply the opportunity to begin again, this time more intelligently.", author: "Henry Ford"}, 
+  {text: "We are all born ignorant, but one must work hard to remain stupid.", author: "Benjamin Franklin"}, 
+  {text: "Imagination will often carry us to worlds that never were. But without it we go nowhere.", author: "Carl Sagan"}, 
+  {text: "Adults are obsolete children.", author: "Dr. Seuss"}, 
+  {text: "Good artists copy, great artists steal.", author: "Pablo Picasso"},
+  {text: "The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.", author: "Mark Twain"}
+
+  ])
