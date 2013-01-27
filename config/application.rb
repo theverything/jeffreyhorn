5aa7d15359967f4aca905f2af14ce97aa22b4ae1
Jeff Horn <theverything@BigMac.local>
2013-01-27 13:24:58 -0800

    add jquery cdn

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Quotes
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #jquery CDN
+    config.assets.precompile += ['jquery.js']
   end
 end
