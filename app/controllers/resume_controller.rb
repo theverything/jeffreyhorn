fa8372c774cc07d6864bd8a1e204f5a75bbb1454
theverything <j3ffhorn@gmail.com>
2013-04-14 20:07:25 -0700

    use github resume
    using resume from github rater than parse locally

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,5 +1,16 @@
+require 'redcarpet'
+require 'open-uri'
+
 class ResumeController < ApplicationController
   def index
-   
+    markdown_options = { autolink: true, space_after_headers: true }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, markdown_options)
+    @markdown = markdown.render(git_resume.read)
+  end
+
+  private
+
+  def git_resume
+    open("https://raw.github.com/theverything/Jeffrey_Horn_Resume/master/markdowm_resume.md")
   end
 end
